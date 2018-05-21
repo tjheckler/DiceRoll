919958fc6624db0fe7dbb9b363b26d943235b823
tjheckler <timothyheckler@gmail.com>
2018-05-21 14:00:21 -0500

    RollDie

--- app/controllers/GameController.java
+++ app/controllers/GameController.java
@@ -20,15 +20,29 @@ public class GameController extends Controller
         this.formFactory = formFactory;
     }
 
-    public Result postDice()
+    public Result postDice1()
     {
         DynamicForm form = formFactory.form().bindFromRequest();
         int dieSize = Integer.parseInt(form.get("DieSize"));
-        Random random = new Random();
-
-        int value = random.nextInt(dieSize + 1);
-
-        return ok(views.html.rolldice.render(value));
+        //int howMany = Integer.parseInt(form.get("die1"));
+        Die die1 = new Die(dieSize);
+        return ok(views.html.rolldice.render(die1));
+    }
+    public Result postDice2()
+    {
+        DynamicForm form = formFactory.form().bindFromRequest();
+        int dieSize = Integer.parseInt(form.get("DieSize"));
+        //int howMany = Integer.parseInt(form.get("die2"));
+        Die die2 = new Die(dieSize);
+        return ok(views.html.rolldice.render(die2));
+    }
+    public Result postDice3()
+    {
+        DynamicForm form = formFactory.form().bindFromRequest();
+        int dieSize = Integer.parseInt(form.get("DieSize"));
+        //int howMany = Integer.parseInt(form.get("die3"));
+        Die die3 = new Die(dieSize);
+        return ok(views.html.rolldice.render(die3));
     }
 
     public Result getStart()
--- app/models/Die.java
+++ app/models/Die.java
@@ -1,8 +1,30 @@
 package models;
 
 
+import java.util.Random;
 
 public class Die
 {
+    private int size;
+    //private int howMany;
+    private int value;
 
+    public Die(int size)
+    {
+        this.size = size;
+        //this.howMany = howMany;
+        roll();
+    }
+
+    public void roll()
+    {
+        Random random = new Random();
+         value = random.nextInt(size + 1);
+
+    }
+
+    public int getValue()
+    {
+        return value;
+    }
 }
